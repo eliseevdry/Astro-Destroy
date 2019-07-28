734d583e9d2ba16e1e50a42cf1ed7937d8a837f5
eliseevdry <47399611+eliseevdry@users.noreply.github.com>
2019-07-28 21:28:43 +0500

    2

--- venv/Include/Astro-Destroy.py
+++ venv/Include/Astro-Destroy.py
@@ -1,8 +1,38 @@
 
 from livewires import games
-import  random
+import math, random
 
 games.init(screen_width=640, screen_height=480, fps=50)
+
+
+class Ship(games.Sprite):
+    """ Корабль игрока. """
+    image = games.load_image("ship.bmp")
+    ROTATION_STEP = 3
+    VALIOCITY_STEP = .03
+    sound = games.load_sound("thrust.wav")
+
+    def update(self):
+        """ Вращает корабль при нажатии клавиш со стрелками. """
+        if games.keyboard.is_pressed(games.K_LEFT):
+            self.angle -= Ship.ROTATION_STEP
+        if games.keyboard.is_pressed(games.K_RIGHT):
+            self.angle += Ship.ROTATION_STEP
+        if games.keyboard.is_pressed(games.K_UP):
+            Ship.sound.play()
+            # изменение горизонтальной и верикальной скорости корабля с учетом угла поворота
+            angle = self.angle * math.pi / 180  # преобразование в радианы
+            self.dx += Ship.VALIOCITY_STEP * math.sin(angle)
+            self.dy += Ship.VALIOCITY_STEP * -math.cos(angle)
+        # корабль будет огибать экран
+        if self.top > games.screen.height:
+            self.bottom = 0
+        if self.bottom < 0:
+            self.top = games.screen.height
+        if self.left > games.screen.width:
+            self.right = 0
+        if self.right < 0:
+            self.left = games.screen.width
 
 
 class Asteroid(games.Sprite):
@@ -49,6 +79,12 @@ def main():
         size = random.choice([Asteroid.SMALL, Asteroid.MEDIUM, Asteroid.LARGE])
         new_asteroid = Asteroid(x=x, y=y, size=size)
         games.screen.add(new_asteroid)
+    # создаем корабль
+    the_ship = Ship(image=Ship.image,
+                    x=games.screen.width/2,
+                    y=games.screen.height/2)
+    games.screen.add(the_ship)
+
 
     games.screen.mainloop()
 
