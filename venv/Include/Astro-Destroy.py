0b3b3e01b308e7afd112fe6a304c8b617e0d33af
eliseevdry <47399611+eliseevdry@users.noreply.github.com>
2019-07-28 20:57:10 +0500

    1

--- venv/Include/Astro-Destroy.py
+++ venv/Include/Astro-Destroy.py
@@ -1,31 +1,55 @@
 
 from livewires import games
+import  random
 
 games.init(screen_width=640, screen_height=480, fps=50)
 
 
-class Ship(games.Sprite):
-    """ Подвижный космический корабль """
+class Asteroid(games.Sprite):
+    """ Астероид, прямолинейно движущийся по экрану """
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+    images = {SMALL: games.load_image("asteroid_small.bmp"),
+              MEDIUM: games.load_image("asteroid_med.bmp"),
+              LARGE: games.load_image("asteroid_big.bmp")}
+    SPEED = 2
+
+    def __init__(self, x, y, size):
+        """ Инициализирует спрайт с изображением астероида """
+        super(Asteroid, self).__init__(
+            image=Asteroid.images[size],
+            x=x,
+            y=y,
+            dx=random.choice([1, -1]) * Asteroid.SPEED * random.random()/size,
+            dy=random.choice([1, -1]) * Asteroid.SPEED * random.random() / size,
+        )
+        self.size = size
+
     def update(self):
-        """ Перемещает корабль определенным образом, исходя из нажатых клавиш """
-        if games.keyboard.is_pressed(games.K_w):
-            self.y -= 1
-        if games.keyboard.is_pressed(games.K_s):
-            self.y += 1
-        if games.keyboard.is_pressed(games.K_a):
-            self.x -= 1
-        if games.keyboard.is_pressed(games.K_d):
-            self.x += 1
+        """ Заставляет астероид обогнуть экран """
+        if self.top > games.screen.height:
+            self.bottom = 0
+        if self.bottom < 0:
+            self.top = games.screen.height
+        if self.left > games.screen.width:
+            self.right = 0
+        if self.right < 0:
+            self.left = games.screen.width
 
 
 def main():
-    nebula_image = games.load_image("nebula.jpg", transparent=False)
+    # назначаем фоновую картинку
+    nebula_image = games.load_image("nebula.jpg")
     games.screen.background = nebula_image
-    ship_image = games.load_image("ship.bmp")
-    the_ship = Ship(image=ship_image,
-                    x=games.screen.width/2,
-                    y=games.screen.height/2)
-    games.screen.add(the_ship)
+    # создаем 8 астероидов
+    for i in range(8):
+        x = random.randrange(games.screen.width)
+        y = random.randrange(games.screen.height)
+        size = random.choice([Asteroid.SMALL, Asteroid.MEDIUM, Asteroid.LARGE])
+        new_asteroid = Asteroid(x=x, y=y, size=size)
+        games.screen.add(new_asteroid)
+
     games.screen.mainloop()
 
 
